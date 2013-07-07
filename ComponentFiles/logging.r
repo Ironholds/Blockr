c91baed6d5f75568bc1c3f8e86cc9cbbc7ad1b17
Oliver Keyes <ironholds@gmail.com>
2013-07-07 22:59:45 +0100

    Rework to pull from functions and avoid duping

--- ComponentFiles/logging.r
+++ ComponentFiles/logging.r
@@ -23,49 +23,10 @@ logging.fun <- function(){
           AND logging.log_action = 'block';"
   )
   
-  #Interpret the outcome
-  parse_data.fun <- function(x){
-    
-    #How many items shall we sample from each month?
-    samplesize <- sample_size(x = x,
-                              variable = "block_timestamp",
-                              percentage = 0.20)
-    
-    #Run regexes across rationales
-    parsed_data.df <- regex.fun(input_data = x)
-    
-    #Take the input, generate yearly totals too.
-    data_by_year.df <- parsed_data.df
-    data_by_year.df$block_timestamp <- substring(data_by_year.df$block_timestamp,1,4)
-    data_by_year.df <- ddply(.data = data_by_year.df,
-                             .var = "block_timestamp",
-                             .fun = function(x){
-                              
-                              return(colSums(x[,2:7]))
-                             }
-    )
-    
-    #Melt datasets
-    melted_data_month.df <- melt(parsed_data.df, id.vars = 1, measure.vars = 2:6)
-    melted_data_year.df <- melt(data_by_year.df, id.vars = 1, measure.vars = 2:6)
-    
-    #Export it
-    melted_file_path <- file.path(getwd(),"Output","logging_regex_matches_monthly.tsv")
-    write.table(melted_data_month.df, file = melted_file_path, col.names = TRUE,
-                row.names = FALSE, sep = "\t", quote = FALSE)
-    melted_file_path <- file.path(getwd(),"Output","logging_regex_matches_yearly.tsv")
-    write.table(melted_data_year.df, file = melted_file_path, col.names = TRUE,
-                row.names = FALSE, sep = "\t", quote = FALSE)
-    
-    #List
-    to.return <- list(melted_data_month.df,melted_data_year.df)
-    
-    #Return it
-    return(to.return)
-  }
+
   
-  #Run
-  regex_matches.list <- parse_data.fun(x = query.df)
+  #Run parse_data.fun
+  regex_matches.list <- parse_data.fun(x = query.df, tablename = "logging")
   
   #Graph the outcome
   graphing.fun <- function(x){
@@ -74,42 +35,28 @@ logging.fun <- function(){
     monthly_data.df <- as.data.frame(x[1])
     yearly_data.df <- as.data.frame(x[2])
     
-    #For each one, a simple line-and-point graph.
-    line_graph_monthly <- ggplot(monthly_data.df, aes(block_timestamp, value)) + 
-      geom_freqpoly(aes(group = variable, colour = variable), stat = "identity") +
-      labs(x = "Year", y = "Number of users") +
-      ggtitle("Block rationales on the English-language Wikipedia, by month (2006-2012)") +
-      scale_x_discrete(expand = c(0,0)) +
-      scale_y_continuous(expand = c(0,0)) +
-      theme(axis.text.x = element_blank(), axis.ticks.x = element_blank())
-    
+    #A simple line-and-point graph.
     line_graph_yearly <- ggplot(yearly_data.df, aes(block_timestamp, value)) + 
       geom_freqpoly(aes(group = variable, colour = variable), stat = "identity") +
       labs(x = "Year", y = "Number of users") +
-      ggtitle("Block rationales on the English-language Wikipedia, by year (2006-2012)") +
+      ggtitle("Block rationales on the English-language Wikipedia, by year (2006-2012) - logging table") +
       scale_x_discrete(breaks = 2006:2012, expand = c(0,0)) +
       scale_y_continuous(expand = c(0, 0)) +
       theme(axis.text.x = element_text(angle = 90, hjust = 1))
     
     #print
-    ggsave(filename = file.path(getwd(),"Output", "logging_regex_matches_by_month.png", sep = "")),
-      plot = line_graph_monthly,
-      width = 8,
-      height = 8,
-      units = "in")
-    ggsave(filename = file.path(getwd(),"Output", "logging_regex_matches_by_year.png", sep = "")),
+    ggsave(filename = file.path(getwd(),"Output", "logging_regex_matches_by_year.png"),
       plot = line_graph_yearly,
       width = 8,
       height = 8,
       units = "in")
     
-    #As we can see, that's probably not that useful for data by month.
-    #Perhaps some regression would work better?
+    #Some regression, sir?
     regression_graph_monthly <- ggplot(monthly_data.df,aes(x = block_timestamp,y = value, colour = variable))+
       geom_point(shape=3) +
       geom_smooth(method = lm, se = TRUE, aes(group= variable)) +
       labs(x = "Year", y = "Number of users") +
-      ggtitle("Block rationales on the English-language Wikipedia, by month (2006-2012)") +
+      ggtitle("Block rationales on the English-language Wikipedia, by month (2006-2012) - logging table") +
       scale_x_discrete(expand = c(0,0)) +
       scale_y_continuous(expand = c(0,0)) +
       theme(axis.text.x = element_blank(), axis.ticks.x = element_blank())
@@ -117,6 +64,38 @@ logging.fun <- function(){
     #Print
     ggsave(filename = file.path(getwd(),"Output", "logging_regex_matches_linear_regression.png"),
       plot = regression_graph_monthly,
+      width = 8,
+      height = 8,
+      units = "in")
+    
+    #The dataset makes it difficult to properly show everything. Let's graph some subsets
+    regression_graph_monthly_badfaith <- ggplot(monthly_data.df[monthly_data.df$variable %in% c("Bad.faith","spam"),],
+      aes(x = block_timestamp,y = value, colour = variable))+
+      geom_point(shape=3) +
+      geom_smooth(method = lm, se = TRUE, aes(group= variable)) +
+      labs(x = "Year", y = "Number of users") +
+      ggtitle("Spam and bad-faith blocks on the English-language Wikipedia, by month (2006-2012) - logging table") +
+      scale_x_discrete(expand = c(0,0)) +
+      scale_y_continuous(expand = c(0,0)) +
+      theme(axis.text.x = element_blank(), axis.ticks.x = element_blank())
+      
+    regression_graph_monthly_other <- ggplot(monthly_data.df[!monthly_data.df$variable %in% c("Bad.faith","spam"),],
+      aes(x = block_timestamp,y = value, colour = variable))+
+      geom_point(shape=3) +
+      geom_smooth(method = lm, se = TRUE, aes(group= variable)) +
+      labs(x = "Year", y = "Number of users") +
+      ggtitle("Other blocks on the English-language Wikipedia, by month (2006-2012) - logging table") +
+      scale_x_discrete(expand = c(0,0)) +
+      scale_y_continuous(expand = c(0,0)) +
+      theme(axis.text.x = element_blank(), axis.ticks.x = element_blank())
+    
+    ggsave(filename = file.path(getwd(),"Output", "logging_linear_regression_badfaith.png"),
+      plot = regression_graph_monthly_badfaith,
+      width = 8,
+      height = 8,
+      units = "in")
+    ggsave(filename = file.path(getwd(),"Output", "logging_linear_regression_other.png"),
+      plot = regression_graph_monthly_other,
       width = 8,
       height = 8,
       units = "in")
--- functions.r
+++ functions.r
@@ -30,180 +30,97 @@ sql.fun <- function(query_statement){
   return(output)
 }
 
-#Function to take a dataset and run the regexes over it.
-regex.fun <- function(input_data){
-  
-  #Create list of regexes
-  regex_list <- list(c("(spam|advertis(ement|ing)|promot(e|ion))"),
-                     c("(arb(itration|com)|defamat(ion|ory)|vandal(|ism)|disrupti(ve|on)|WP:BLP|troll|attack|bad faith|harr?ass?(ment)|abus(e|ive)|vau(|block)|suppress|stalk|phish(ing|)|(death|legal) threat|biographies of living|hoax|rac(ist|ial)|copy(right|vio)|threatban|nonsense|(in|un|)civil(ity)|WP:VAND|WP:LEGAL|edit( |-)war(ring)|schoolblock|revert rule|\\drr|deliberately triggering|outing|anonblock|battle(|field|ground)|topic ban)"),
-                     c("((?-i)LTA(?i)|sock|eva(de|sion|ding)|JarlaxleArtemis|grawp|WP:LTA|term abuse|multiple accounts|checkuser|MascotGuy|Jessica Liao|Grundle2600|Swale|sleeper|willy|puppet|\\{\\{WoW\\}\\}|reincarnaton|ararat|CU block|banned)"),
-                     c("(username|uw-(uhblock|softestblock)|(softer|cause|u)block|impost(or|er)|too similar|similar to existing user or recent meme|\\{\\{unb|contact an administrator for verification|impersonat(or|ion|ing))"),
-                     c("(torblock|blocked proxy|webhostblock|\\{\\{tor)"))
-  
-  output_data.df <- ddply(.data = input_data,
-                          .var = "block_timestamp",
-                          .fun = function(x){
-                            
-                            #sample from the subset to produce normalised data, operating off the value of samplesize
-                            x <- dfsample(df = x, size = samplesize)
-                            
-                            #Pin input data
-                            to_run.df <- x
-                            
-                            #Create empty vector.
-                            to_return.vec <- vector()
-                            
-                            #Grab length
-                            reg_length <- length(regex_list)
-                            
-                            #And now we loop.
-                            for(i in (1:reg_length)){
-                              
-                              #Run regexes
-                              grepvec <- grepl(pattern = regex_list[i],
-                              x = to_run.df$reason,
-                              perl = TRUE,
-                              ignore.case = TRUE)
-                              
-                              #Number of rows that match
-                              to_return.vec[i] <- sum(grepvec)
-                              
-                              #Non-matches
-                              to_run.df <- to_run.df[!grepvec,]
-                            }
-                            
-                            #Include non-matches
-                            to_return.vec[reg_length+1] <- nrow(to_run.df)
-                            
-                            #Return
-                            return(to_return.vec)}
-                          )
-  
-  #Rename
-  output_data.df <- rename(output_data.df,c("V1" = "spam","V2" = "bad.faith","V3" = "Sockpuppetry", "V4" = "Username.problems", "V5" = "Proxies", "V6" = "Misc"))
-
-  #Return
-  return(output_data.df)
-}
-
-#Function to take the dataset and run the regexes over it.
-rege2x.fun <- function(x, graphname, fileprefix, is.testing){
-  
-  #Retrieve an appropriate sample size
-  samplesize <- sample_size(x = x,
-    variable = "block_timestamp",
-    percentage = 0.20)
-  
-  #Retrieve sample of users indefinitely blocked, pivoting on block date. 
-  blocked.df <- ddply(.data = x,
-    .variables = "block_timestamp",
-    .fun = function(x){
+parse_data.fun <- function(x, tablename){
+    
+    #Regex function
+    regex.fun <- function(input_data){
       
-      #sample from the subset to produce normalised data, operating off the value of samplesize
-      sample.df <- dfsample(df = x, size = samplesize)
+      #How many items shall we sample?
+      samplesize <- sample_size(x = input_data,
+                                  variable = "block_timestamp",
+                                  percentage = 0.20)
       
-      return(sample.df)
-      #wrapping function to iterate over
-      metafun.fun <- function(item, reg_pattern){
-        
-        #Run regexes
-        grepvec <- grepl(pattern = reg_pattern,
-        x = item$reason,
-        perl = TRUE,
-        ignore.case = TRUE)
-        
-        #Number of rows that match
-        nummatch <- length(grepvec[TRUE])
-        
-        #Grab matches
-        match <- item[grepvec,]
-        
-        #Grab non-matches
-        nonmatch <- item[!grepvec,]
-        
-        #Include regex name in matches
-        match$regex_matched <- as.character(substitute(reg_pattern))
-        
-        #Bind as a list
-        to.return <- list(nummatch,match,nonmatch)
-        
-        #Return
-        return(to.return)
-      }
-
-      #If we're testing, export matches/non-matches for handcoding
-      if(is.testing == TRUE){
-        
-        #Stitch
-        matches.df <- rbind(
-          as.data.frame(spam[2]),
-          as.data.frame(disruption[2]),
-          as.data.frame(socks[2]),
-          as.data.frame(usernames[2]),
-          as.data.frame(proxies[2])
-        )
+      #Create list of regexes
+      regex_list <- list(c("(spam|advertis(ement|ing)|promot(e|ion))"),
+                         c("(arb(itration|com)|defamat(ion|ory)|vandal(|ism)|disrupti(ve|on)|WP:BLP|troll|attack|bad faith|harr?ass?(ment)|abus(e|ive)|vau(|block)|suppress|stalk|phish(ing|)|(death|legal) threat|biographies of living|hoax|rac(ist|ial)|copy(right|vio)|threatban|nonsense|(in|un|)civil(ity)|WP:VAND|WP:LEGAL|edit( |-)war(ring)|schoolblock|revert rule|\\drr|deliberately triggering|outing|anonblock|battle(|field|ground)|topic ban)"),
+                         c("((?-i)LTA(?i)|sock|eva(de|sion|ding)|JarlaxleArtemis|grawp|WP:LTA|term abuse|multiple accounts|checkuser|MascotGuy|Jessica Liao|Grundle2600|Swale|sleeper|willy|puppet|\\{\\{WoW\\}\\}|reincarnaton|ararat|CU block|banned)"),
+                         c("(username|uw-(uhblock|softestblock)|(softer|cause|u)block|impost(or|er)|too similar|similar to existing user or recent meme|\\{\\{unb|contact an administrator for verification|impersonat(or|ion|ing))"),
+                         c("(torblock|blocked proxy|webhostblock|\\{\\{tor)"))
       
-        #Write matches
-        tsv_wrapper(x = matches.df,
-          file = file.path(
-            getwd(), "Output", paste(fileprefix,"_matches.tsv", sep = "")
-          )
-        )
-        
-        #Write non-matches
-        tsv_wrapper(x = as.data.frame(proxies[3]),
-          file = file.path(
-            getwd(), "Output", paste(fileprefix,"_non_matches.tsv", sep = "")
-          )
-        )
-      }
-        
-      #Whether we're testing or not, bind the results together and return them.
-      results <- c(as.numeric(spam[1]),as.numeric(disruption[1]),as.numeric(socks[1]),as.numeric(usernames[1]),as.numeric(proxies[1]),as.numeric(nrow(as.data.frame(proxies[3]))))
+      output_data.df <- ddply(.data = input_data,
+                              .var = "block_timestamp",
+                              .fun = function(x, samplesize){
+                                
+                                #sample from the subset to produce normalised data, operating off the value of samplesize
+                                x <- dfsample(df = x, size = samplesize)
+                                
+                                #Pin input data
+                                to_run.df <- x
+                                
+                                #Create empty vector.
+                                to_return.vec <- vector()
+                                
+                                #Grab length
+                                reg_length <- length(regex_list)
+                                
+                                #And now we loop.
+                                for(i in (1:reg_length)){
+                                  
+                                  #Run regexes
+                                  grepvec <- grepl(pattern = regex_list[i],
+                                  x = to_run.df$reason,
+                                  perl = TRUE,
+                                  ignore.case = TRUE)
+                                  
+                                  #Number of rows that match
+                                  to_return.vec[i] <- sum(grepvec)
+                                  
+                                  #Non-matches
+                                  to_run.df <- to_run.df[!grepvec,]
+                                }
+                                
+                                #Include non-matches
+                                to_return.vec[reg_length+1] <- nrow(to_run.df)
+                                
+                                #Return
+                                return(to_return.vec)}
+                              )
       
-      #Bind them all together in a vector
-      output <- as.vector(results)
-          
+      #Rename
+      output_data.df <- rename(output_data.df,c("V1" = "spam","V2" = "bad.faith","V3" = "Sockpuppetry", "V4" = "Username.problems", "V5" = "Proxies", "V6" = "Misc"))
+    
       #Return
-      return(output)
+      return(output_data.df)
     }
-  )
-
-  #Make non-hideous
-  blocked.df <- rename(blocked.df,
-    replace = c("V1" = "spam",
-      "V2" = "disruption",
-      "V3" = "sockpuppet",
-      "V4" = "username",
-      "V5" = "proxies",
-      "V6" = "misc"
+    #Run regexes across rationales
+    parsed_data.df <- regex.fun(input_data = x)
+    
+    #Take the input, generate yearly totals too.
+    data_by_year.df <- parsed_data.df
+    data_by_year.df$block_timestamp <- substring(data_by_year.df$block_timestamp,1,4)
+    data_by_year.df <- ddply(.data = data_by_year.df,
+                             .var = "block_timestamp",
+                             .fun = function(x){
+                              
+                              return(colSums(x[,2:7]))
+                             }
     )
-  )
-
-  #Melt it so that ggplot2 can accept it as a faceted/stacked dataset
-  melted.df <- melt(blocked.df, id.vars = 1, measure.vars = 2:6)
-  
-  #Write dataset
-  tsv_wrapper(x = melted.df,
-    file = file.path(
-      getwd(), "Output", paste(fileprefix,"_blocked.tsv", sep = "")
-    )
-  )
-  
-  #A line graph of blocking rationales over time
-  line.graph.blocked <- ggplot(melted.df, aes(block_timestamp, value)) + 
-    geom_freqpoly(aes(group = variable, colour = variable), stat = "identity") +
-    labs(x = "Year", y = "Number of users") +
-    ggtitle(graphname) +
-    scale_x_discrete(breaks = 2006:2012) +
-    scale_y_continuous(expand = c(0, 0)) +
-    theme(axis.text.x = element_text(angle = 90, hjust = 1))
-
-  #Print
-  ggsave(filename = file.path(getwd(),"Output", paste(fileprefix,"_blocked_line.png", sep = "")),
-    plot = line.graph.blocked,
-    width = 8,
-    height = 8,
-    units = "in")
-}
- 
+    
+    #Melt datasets
+    melted_data_month.df <- melt(parsed_data.df, id.vars = 1, measure.vars = 2:6)
+    melted_data_year.df <- melt(data_by_year.df, id.vars = 1, measure.vars = 2:6)
+    
+    #Export it
+    melted_file_path <- file.path(getwd(),"Output",paste(tablename,"regex_matches_monthly.tsv",sep = "_"))
+    write.table(melted_data_month.df, file = melted_file_path, col.names = TRUE,
+                row.names = FALSE, sep = "\t", quote = FALSE)
+    melted_file_path <- file.path(getwd(),"Output",paste(tablename,"regex_matches_yearly.tsv",sep = "_"))
+    write.table(melted_data_year.df, file = melted_file_path, col.names = TRUE,
+                row.names = FALSE, sep = "\t", quote = FALSE)
+    
+    #List
+    to.return <- list(melted_data_month.df,melted_data_year.df)
+    
+    #Return it
+    return(to.return)
+  }
