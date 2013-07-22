7ad205e1d1e954ab668f7c5d9157b34d8871a8a4
ironholds <ironholds@LauraKidd.ironholds.org>
2013-07-22 16:19:56 -0700

    bugfix

--- ComponentFiles/ipblock.r
+++ ComponentFiles/ipblock.r
@@ -52,7 +52,7 @@ ipblock.fun <- function(){
       width = 8,
       height = 8,
       units = "in")
-    ggsave(filename = file.path(getwd(),"Output", "ipblocks_regex_matches_by_year.png"),
+    ggsave(filename = file.path(getwd(),"Output", "ipblocks_regex_matches_by_month.png"),
       plot = line_graph_monthly,
       width = 8,
       height = 8,
