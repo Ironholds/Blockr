01530a7cd3422f94c261b6caa1b5ddcf1e5972f1
Oliver Keyes <ironholds@gmail.com>
2013-08-11 15:54:25 +0100

    Finish hand-coding generation

--- TestComponents/retrieve.r
+++ TestComponents/retrieve.r
@@ -1,3 +1,18 @@
+retrieve.fun <- function(){
+  
+  #Retrieve blocks of logged-in users.
+  query.df <- sql.fun(query_statement = "
+        SELECT
+          substring(logging.log_timestamp,1,6) AS block_timestamp,
+          logging.log_comment AS reason,
+          user.user_id AS userid
+        FROM logging INNER JOIN user ON logging.log_title = user.user_name
+        WHERE logging.log_timestamp BETWEEN 20060101010101 AND 20121231235959
+          AND logging.log_type = 'block'
+          AND logging.log_action = 'block'
+          AND logging.log_params NOT LIKE '%indefinite%';"
+  )
+  
   #Function to output data for hand-coding
   usertest.fun <- function(x){
     
@@ -37,8 +52,25 @@
     #Save non-matches and matches both to file.
     usertest_file_path <- file.path(getwd(),"Output","blocks_with_match_status.tsv")
     write.table(output.df, file = usertest_file_path, col.names = TRUE,
-                row.names = FALSE, sep = "\t", quote = FALSE)
+                row.names = FALSE, sep = "\t", quote = TRUE, qmethod = "double")
   }
   
-  #Run
-  usertest.fun(registered.df)
+  #Random sampling
+  sample.df <- ddply(.data = query.df,
+                     .var = "block_timestamp",
+                     .fun = function(x){
+                       
+                       #Randomly sample
+                       to_output.df <- dfsample(x, 250)
+                       
+                       #Return
+                       return(to_output.df)
+                     }
+  )
+  
+  #Run regexes and export
+  usertest.fun(sample.df)
+}
+
+#Run
+retrieve.fun()
