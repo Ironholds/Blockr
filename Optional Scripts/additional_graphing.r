91c697dc903f5a3bd9aec5ee0ff0ad43cfe16369
Oliver Keyes <ironholds@gmail.com>
2013-09-09 00:29:40 +0100

    graphing

--- Optional Scripts/additional_graphing.r
+++ Optional Scripts/additional_graphing.r
@@ -63,145 +63,149 @@ additional_graphs.fun <- function(){
     #Return
     return(bound.df)
   }
-     
-  #Run
-  data.df <- data_read.fun(table = "logging", usergroup = "registered")
-  
-  #Linear regression; what's the relationship between AV blocks and registrations?
-  regression_graph_registrations <- ggplot(data.df, aes(x = blocks,y = registrations))+
-    geom_point(shape=3) +
-    geom_smooth(method = "lm", se=TRUE, formula = y ~ x) +
-    geom_text(aes(x = 500, y = 250000, label = trickstr::r2_display(lm = lm(registrations ~ blocks, data.df))), parse = TRUE) +
-    labs(x = "bad-faith blocks (monthly)", y = "Registrations") +
-    ggtitle("Relationship between blocks and registrations, by month \n(2006-2013)") +
-    scale_x_continuous(expand = c(0.01,0.01)) +
-    scale_y_continuous(expand = c(0.01,0.01))
+       
+  graph.fun <- function(){
     
-  #Print
-  ggsave(filename = file.path(getwd(),"Graphs","registrations_versus_blocks.png"),
-         plot = regression_graph_registrations,
-         width = 8,
-         height = 8,
-         units = "in")
+    data.df <- data_read.fun(table = "logging", usergroup = "registered")
+    
+    #Linear regression; what's the relationship between AV blocks and registrations?
+    regression_graph_registrations <- ggplot(data.df, aes(x = blocks,y = registrations))+
+      geom_point(shape=3) +
+      geom_smooth(method = "lm", se=TRUE, formula = y ~ x) +
+      geom_text(aes(x = 500, y = 250000, label = trickstr::r2_display(lm = lm(registrations ~ blocks, data.df))), parse = TRUE) +
+      labs(x = "bad-faith blocks (monthly)", y = "Registrations") +
+      ggtitle("Relationship between blocks and registrations, by month \n(2006-2013)") +
+      scale_x_continuous(expand = c(0.01,0.01)) +
+      scale_y_continuous(expand = c(0.01,0.01))
+      
+    #Print
+    ggsave(filename = file.path(getwd(),"Graphs","registrations_versus_blocks.png"),
+           plot = regression_graph_registrations,
+           width = 8,
+           height = 8,
+           units = "in")
+    
+    #R2 and other data
+    sink(file = file.path(getwd(),"Metadata", "registrations_versus_blocks.txt"))
+    print(summary(lm(formula = registrations ~  blocks, data = data.df,)))
+    sink()
+    
+    #Regression
+    regression_graph_registrations_w_edits <- ggplot(data.df, aes(x = blocks,y = registrations_with_edits))+
+      geom_point(shape=3) +
+      geom_smooth(method = "lm", se=TRUE, formula = y ~ x) +
+      geom_text(aes(x = 750, y = 120000, label = trickstr::r2_display(lm = lm(registrations_with_edits ~ blocks, data.df))), parse = TRUE) +
+      labs(x = "bad-faith blocks (monthly)", y = "Registrations") +
+      ggtitle("Relationship between blocks and registrations of accounts that edit\n by month (2006-2013)") +
+      scale_x_continuous(expand = c(0.01,0.01)) +
+      scale_y_continuous(expand = c(0.01,0.01))
+    
+    #Print
+    ggsave(filename = file.path(getwd(),"Graphs","registrations_w_edits_versus_blocks.png"),
+           plot = regression_graph_registrations_w_edits,
+           width = 8,
+           height = 8,
+           units = "in")
+    
+    #R2 and other data
+    sink(file = file.path(getwd(),"Metadata", "registrations_w_edits_versus_blocks.txt"))
+    print(summary(lm(formula = registrations_with_edits ~  blocks, data = data.df,)))
+    sink()
+    
+    #Subset data
+    data.df <- data.df[data.df$timestamp >= 200903,]
+    
+    #Regression
+    regression_graph_anonymous_editfilter_hits <- ggplot(data.df, aes(x = blocks,y = anonymous_editfilter_hits))+
+      geom_point(shape=3) +
+      geom_smooth(method = "lm", se=TRUE, formula = y ~ x) +
+      geom_text(aes(x = 400, y = 100000, label = trickstr::r2_display(lm = lm(anonymous_editfilter_hits ~ blocks, data.df))), parse = TRUE) +
+      labs(x = "bad-faith blocks (monthly)", y = "EditFilter hits on anonymous edits") +
+      ggtitle("Relationship between blocks and editfilter hits \n anonymous hits, by month (2009-2013)") +
+      scale_x_continuous(expand = c(0.01,0.01)) +
+      scale_y_continuous(expand = c(0.01,0.01))
+    
+    #Print
+    ggsave(filename = file.path(getwd(),"Graphs","anon_abusefilter_hits_versus_blocks.png"),
+           plot = regression_graph_anonymous_editfilter_hits,
+           width = 8,
+           height = 8,
+           units = "in")
+    
+    #R2 and other data
+    sink(file = file.path(getwd(),"Metadata", "anon_abusefilter_hits_versus_blocks.txt"))
+    print(summary(lm(formula = anonymous_editfilter_hits ~  blocks, data = data.df,)))
+    sink()  
+    
+    #Regression
+    regression_graph_anonymous_editfilter_prohibits <- ggplot(data.df, aes(x = blocks,y = anonymous_editfilter_prohibits))+
+      geom_point(shape=3) +
+      geom_smooth(method = "lm", se=TRUE, formula = y ~ x) +
+      geom_text(aes(x = 500, y = 40000, label = trickstr::r2_display(lm = lm(anonymous_editfilter_prohibits ~ blocks, data.df))), parse = TRUE) +
+      labs(x = "bad-faith blocks (monthly)", y = "EditFilter prohibits on anonymous edits") +
+      ggtitle("Relationship between blocks and editfilter prohibits \n anonymous prohibits, by month (2009-2013)") +
+      scale_x_continuous(expand = c(0.01,0.01)) +
+      scale_y_continuous(expand = c(0.01,0.01))
   
-  #R2 and other data
-  sink(file = file.path(getwd(),"Metadata", "registrations_versus_blocks.txt"))
-  print(summary(lm(formula = registrations ~  blocks, data = data.df,)))
-  sink()
+    #Print
+    ggsave(filename = file.path(getwd(),"Graphs","anon_abusefilter_prohibits_versus_blocks.png"),
+           plot = regression_graph_anonymous_editfilter_prohibits,
+           width = 8,
+           height = 8,
+           units = "in")
+    
+    #R2 and other data
+    sink(file = file.path(getwd(),"Metadata", "anon_abusefilter_prohibits_versus_blocks.png"))
+    print(summary(lm(formula = anonymous_editfilter_prohibits ~  blocks, data = data.df,)))
+    sink()
+    
+    #Regression
+    regression_graph_registered_editfilter_hits <- ggplot(data.df, aes(x = blocks,y = registered_editfilter_hits))+
+      geom_point(shape=3) +
+      geom_smooth(method = "lm", se=TRUE, formula = y ~ x) +
+      geom_text(aes(x = 300, y = 24000, label = trickstr::r2_display(lm = lm(registered_editfilter_hits ~ blocks, data.df))), parse = TRUE) +
+      labs(x = "bad-faith blocks (monthly)", y = "EditFilter hits on registered edits") +
+      ggtitle("Relationship between blocks and editfilter hits \n registered hits, by month (2009-2013)") +
+      scale_x_continuous(expand = c(0.01,0.01)) +
+      scale_y_continuous(expand = c(0.01,0.01))
   
-  #Regression
-  regression_graph_registrations_w_edits <- ggplot(data.df, aes(x = blocks,y = registrations_with_edits))+
-    geom_point(shape=3) +
-    geom_smooth(method = "lm", se=TRUE, formula = y ~ x) +
-    geom_text(aes(x = 750, y = 120000, label = trickstr::r2_display(lm = lm(registrations_with_edits ~ blocks, data.df))), parse = TRUE) +
-    labs(x = "bad-faith blocks (monthly)", y = "Registrations") +
-    ggtitle("Relationship between blocks and registrations of accounts that edit\n by month (2006-2013)") +
-    scale_x_continuous(expand = c(0.01,0.01)) +
-    scale_y_continuous(expand = c(0.01,0.01))
+    #Print
+    ggsave(filename = file.path(getwd(),"Graphs","registered_abusefilter_hits_versus_blocks.png"),
+           plot = regression_graph_registered_editfilter_hits,
+           width = 8,
+           height = 8,
+           units = "in")
+    
+    #R2 and other data
+    sink(file = file.path(getwd(),"Metadata", "registered_abusefilter_hits_versus_blocks.txt"))
+    print(summary(lm(formula = registered_editfilter_hits ~  blocks, data = data.df,)))
+    sink()
+    
+    #Regression
+    regression_graph_registered_editfilter_prohibits <- ggplot(data.df, aes(x = blocks,y = registered_editfilter_prohibits))+
+      geom_point(shape=3) +
+      geom_smooth(method = "lm", se=TRUE, formula = y ~ x) +
+      geom_text(aes(x = 400, y = 6000, label = trickstr::r2_display(lm = lm(registered_editfilter_prohibits ~ blocks, data.df))), parse = TRUE) +
+      labs(x = "bad-faith blocks (monthly)", y = "EditFilter prohibits on registered edits") +
+      ggtitle("Relationship between blocks and editfilter prohibits \n registered prohibits, by month (2009-2013)") +
+      scale_x_continuous(expand = c(0.01,0.01)) +
+      scale_y_continuous(expand = c(0.01,0.01))
+    
+    #Print
+    ggsave(filename = file.path(getwd(),"Graphs","registered_abusefilter_prohibits_versus_blocks.png"),
+           plot = regression_graph_registered_editfilter_prohibits,
+           width = 8,
+           height = 8,
+           units = "in")
+    
+    #R2 and other data
+    sink(file = file.path(getwd(),"Metadata", "registered_abusefilter_prohibits_versus_blocks.txt"))
+    print(summary(lm(formula = registered_editfilter_hits ~  blocks, data = data.df,)))
+    sink()
+  }
   
-  #Print
-  ggsave(filename = file.path(getwd(),"Graphs","registrations_w_edits_versus_blocks.png"),
-         plot = regression_graph_registrations_w_edits,
-         width = 8,
-         height = 8,
-         units = "in")
-  
-  #R2 and other data
-  sink(file = file.path(getwd(),"Metadata", "registrations_w_edits_versus_blocks.txt"))
-  print(summary(lm(formula = registrations_with_edits ~  blocks, data = data.df,)))
-  sink()
-  
-  #Subset data
-  data.df <- data.df[data.df$timestamp >= 200903,]
-  
-  #Regression
-  regression_graph_anonymous_editfilter_hits <- ggplot(data.df, aes(x = blocks,y = anonymous_editfilter_hits))+
-    geom_point(shape=3) +
-    geom_smooth(method = "lm", se=TRUE, formula = y ~ x) +
-    geom_text(aes(x = 400, y = 100000, label = trickstr::r2_display(lm = lm(anonymous_editfilter_hits ~ blocks, data.df))), parse = TRUE) +
-    labs(x = "bad-faith blocks (monthly)", y = "EditFilter hits on anonymous edits") +
-    ggtitle("Relationship between blocks and editfilter hits \n anonymous hits, by month (2009-2013)") +
-    scale_x_continuous(expand = c(0.01,0.01)) +
-    scale_y_continuous(expand = c(0.01,0.01))
-  
-  #Print
-  ggsave(filename = file.path(getwd(),"Graphs","anon_abusefilter_hits_versus_blocks.png"),
-         plot = regression_graph_anonymous_editfilter_hits,
-         width = 8,
-         height = 8,
-         units = "in")
-  
-  #R2 and other data
-  sink(file = file.path(getwd(),"Metadata", "anon_abusefilter_hits_versus_blocks.txt"))
-  print(summary(lm(formula = anonymous_editfilter_hits ~  blocks, data = data.df,)))
-  sink()  
-  
-  #Regression
-  regression_graph_anonymous_editfilter_prohibits <- ggplot(data.df, aes(x = blocks,y = anonymous_editfilter_prohibits))+
-    geom_point(shape=3) +
-    geom_smooth(method = "lm", se=TRUE, formula = y ~ x) +
-    geom_text(aes(x = 500, y = 40000, label = trickstr::r2_display(lm = lm(anonymous_editfilter_prohibits ~ blocks, data.df))), parse = TRUE) +
-    labs(x = "bad-faith blocks (monthly)", y = "EditFilter prohibits on anonymous edits") +
-    ggtitle("Relationship between blocks and editfilter prohibits \n anonymous prohibits, by month (2009-2013)") +
-    scale_x_continuous(expand = c(0.01,0.01)) +
-    scale_y_continuous(expand = c(0.01,0.01))
-
-  #Print
-  ggsave(filename = file.path(getwd(),"Graphs","anon_abusefilter_prohibits_versus_blocks.png"),
-         plot = regression_graph_anonymous_editfilter_prohibits,
-         width = 8,
-         height = 8,
-         units = "in")
-  
-  #R2 and other data
-  sink(file = file.path(getwd(),"Metadata", "anon_abusefilter_prohibits_versus_blocks.png"))
-  print(summary(lm(formula = anonymous_editfilter_prohibits ~  blocks, data = data.df,)))
-  sink()
-  
-  #Regression
-  regression_graph_registered_editfilter_hits <- ggplot(data.df, aes(x = blocks,y = registered_editfilter_hits))+
-    geom_point(shape=3) +
-    geom_smooth(method = "lm", se=TRUE, formula = y ~ x) +
-    geom_text(aes(x = 300, y = 24000, label = trickstr::r2_display(lm = lm(registered_editfilter_hits ~ blocks, data.df))), parse = TRUE) +
-    labs(x = "bad-faith blocks (monthly)", y = "EditFilter hits on registered edits") +
-    ggtitle("Relationship between blocks and editfilter hits \n registered hits, by month (2009-2013)") +
-    scale_x_continuous(expand = c(0.01,0.01)) +
-    scale_y_continuous(expand = c(0.01,0.01))
-
-  #Print
-  ggsave(filename = file.path(getwd(),"Graphs","registered_abusefilter_hits_versus_blocks.png"),
-         plot = regression_graph_registered_editfilter_hits,
-         width = 8,
-         height = 8,
-         units = "in")
-  
-  #R2 and other data
-  sink(file = file.path(getwd(),"Metadata", "registered_abusefilter_hits_versus_blocks.txt"))
-  print(summary(lm(formula = registered_editfilter_hits ~  blocks, data = data.df,)))
-  sink()
-  
-  #Regression
-  regression_graph_registered_editfilter_prohibits <- ggplot(data.df, aes(x = blocks,y = registered_editfilter_prohibits))+
-    geom_point(shape=3) +
-    geom_smooth(method = "lm", se=TRUE, formula = y ~ x) +
-    geom_text(aes(x = 400, y = 6000, label = trickstr::r2_display(lm = lm(registered_editfilter_prohibits ~ blocks, data.df))), parse = TRUE) +
-    labs(x = "bad-faith blocks (monthly)", y = "EditFilter prohibits on registered edits") +
-    ggtitle("Relationship between blocks and editfilter prohibits \n registered prohibits, by month (2009-2013)") +
-    scale_x_continuous(expand = c(0.01,0.01)) +
-    scale_y_continuous(expand = c(0.01,0.01))
-  
-  #Print
-  ggsave(filename = file.path(getwd(),"Graphs","registered_abusefilter_prohibits_versus_blocks.png"),
-         plot = regression_graph_registered_editfilter_prohibits,
-         width = 8,
-         height = 8,
-         units = "in")
-  
-  #R2 and other data
-  sink(file = file.path(getwd(),"Metadata", "registered_abusefilter_prohibits_versus_blocks.txt"))
-  print(summary(lm(formula = registered_editfilter_hits ~  blocks, data = data.df,)))
-  sink()
-  
+  #Run
+  graph.fun()
 }
 
 #Run
