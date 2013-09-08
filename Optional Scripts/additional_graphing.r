d7f2cf9ebdc7668e9a469dbb2a12bbb4978241d4
Oliver Keyes <ironholds@gmail.com>
2013-09-08 19:51:17 +0100

    additional graphing

--- Optional Scripts/additional_graphing.r
+++ Optional Scripts/additional_graphing.r
@@ -10,30 +10,57 @@
 #Wrapping function
 additional_graph.fun <- function(){
   
-  #Read non-context-specific data in
-  registrations.df <- read.delim(file = file.path(getwd(),"Data","registrations.tsv"), as.is = TRUE, header = TRUE)
-  registrations_with_edits.df <- read.delim(file = file.path(getwd(),"Data","registrations_with_edits.tsv"), as.is = TRUE, header = TRUE)
+  data_read.fun <- function(){
   
-  #Take filter hits and subset
-  filter.fun <- function(){
+    #Read non-context-specific data in
+    registrations.df <- read.delim(file = file.path(getwd(),"Data","registrations.tsv"), as.is = TRUE, header = TRUE)
+    registrations_with_edits.df <- read.delim(file = file.path(getwd(),"Data","registrations_with_edits.tsv"), as.is = TRUE, header = TRUE)
+    block_data.df <- subset(read.delim(file = file.path(getwd(),"Data","logging_registered_regex_matches.tsv"), as.is = TRUE, header = TRUE), variable == "bad.edit")
     
-    #Take filter hits
-    editfilters.df <- read.delim(file = file.path(getwd(),"Data","editfilter_hits.tsv"), as.is = TRUE, header = TRUE)
-
-  }
-  
-  #Run
-  filter_data.ls <- filter.fun()
-  
-  #Function to iterate on for comparisons
-  iterate.fun <- function(table,usergroup){
+    #Read edit-filter data in
+    editfilter.df <- read.delim(file = file.path(getwd(),"Data","editfilter_hits.tsv"), as.is = TRUE, header = TRUE)
+    anonymous_hits.df <- editfilter.df[editfilter.df$user_id == 0,]
+    registered_hits.df <- editfilter.df[editfilter.df$user_id > 0,]
     
-    #Read in pertinent data
-    block_data.df <- read.delim(file = file.path(getwd(),"Data",paste(table,usergroup,"regex_matches.tsv",sep = "_")), as.is = TRUE, header = TRUE)
+    #Process
+    filter_process.fun <- function(x){
+      
+      #Grab totals.
+      loose.df <- as.data.frame(table(x$timestamp))
+      nonwarn.df <- x[!x$filter_action == "warn",]
+      grepvec <- grepl(pattern = "blockautopromote", x = nonwarn.df$filter_action, perl = TRUE, ignore.case = TRUE)
+      strict.df <- as.data.frame(table(nonwarn.df[!grepvec,]$timestamp))
+      
+      #Insert NAs
+      bind_NA <- rep(NA,(nrow(block_data.df)-nrow(loose.df)))
+      to_return_loose <- c(bind_NA,loose.df$Freq)
+      to_return_strict <- c(bind_NA,strict.df$Freq)
+      
+      #Return
+      return(data.frame(to_return_loose,to_return_strict))
+    }
     
-    #Subset
-    block_data.df <- block_data.df[block_data.df$variable = "bad.edit",]
+    #Run
+    anonymous_hits.df <- filter_process.fun(anonymous_hits.df)
+    registered_hits.df <- filter_process.fun(registered_hits.df)
     
+    #Bind it all together
+    bound.df <- cbind(block_data.df[,c(1,3)],registrations.df$Freq,registrations_with_edits.df$Freq,anonymous_hits.df,registered_hits.df)
+    
+    #Rename
+    bound.df <- rename(bound.df, replace = c("block_timestamp" = "timestamp", "value" = "blocks", "registrations.df$Freq" = "registrations",
+                                             "registrations_with_edits.df$Freq" = "registrations_with_edits", "to_return_loose" = "anonymous_editfilter_hits",
+                                             "to_return_strict" = "anonymous_editfilter_prohibits"))
+    names(bound.df)[7] <- "registered_editfilter_hits"
+    names(bound.df)[8] <- "registered_editfilter_prohibits"
+    
+    #Save
+    blockr_file_path <- file.path(getwd(),"Data","additional_graphing.tsv")
+    write.table(bound.df, file = blockr_file_path, col.names = TRUE,
+                row.names = FALSE, sep = "\t")
+    
+    #Return
+    return(bound.df)
   }
   
 }
